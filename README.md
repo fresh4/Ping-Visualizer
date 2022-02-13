7e41dc6c55be880317366bcb5b8edf5fe76fe673
fresh4 <fresh1736@gmail.com>
2022-02-12 20:30:37 -0600

    Updated charts to be more readable and reset json data

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 To use this, run `py ping.py`.
 - Enter the ip you want to ping.
 - Enter the file you want to write to:
-    - Files 102.json and 104.json, currently. Clear out those files to reset the data.
+    - Files `102.json` and `104.json`, currently. Clear out those files to reset the data.
+    - `102.json` and `104.json` will update the 102 and 104 graphs respectively in realtime.
--- src/Components/ChartDiv.js
+++ src/Components/ChartDiv.js
@@ -1,12 +1,14 @@
-import { LineChart, Line, XAxis, Y, YAxis, ScatterChart, CartesianGrid, Scatter, Tooltip } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, ScatterChart, CartesianGrid, Scatter, Tooltip, Label } from 'recharts';
 
 export function PingScatter({data, label}) {
     return (
         <div className="App">
             <ScatterChart width={1000} height={250} margin={{left:50, right:20}}>
-                <XAxis unit="s" fontSize={15}/>
+                <CartesianGrid strokeDasharray="3 3" />
+
+                <XAxis unit="m" fontSize={15} interval={59} tickFormatter={(e) => e/60} />
                 <YAxis dataKey="x" unit="ms" fontSize={15}/>
-                <Scatter name={label} data={data} fill="#82ca9d"/>
+                <Scatter name={label} data={data} fill="#82ca9d" />
                 <Tooltip />
             </ScatterChart>
             <h1>{label}</h1>
@@ -17,13 +19,11 @@ export function PingScatter({data, label}) {
 export function PingLine({data, label}) {
     return (
         <div className="App">
-            <LineChart 
-                width={1000} height={400} 
-                data={data}
-            >
-                <Line type="monotone" dataKey="x" stroke="#8884d8" animationDuration={500} dot={false} />
+            <LineChart width={1000} height={400} data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
 
-                <XAxis unit="s" fontSize={15}/>
+                <Line type="monotone" dataKey="x" stroke="#8884d8" animationDuration={500} dot={false} />
+                <XAxis unit="m" fontSize={15} interval={59} tickFormatter={(e) => e/60} />
                 <YAxis dataKey="x" unit="ms" fontSize={15}/>
             </LineChart>
             <h1>{label}</h1>
