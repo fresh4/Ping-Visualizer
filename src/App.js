b067f722fe6579795ddf139c6cc54354fa9e4374
Fresh <fresh1736@gmail.com>
2022-02-14 13:10:08 -0600

    New performant chart option

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import './App.css';
 import { PingScatter } from './Components/ChartDiv';
+import { CanvasJSChart } from 'canvasjs-react-charts'
 import data102 from './pyscripts/102.json'
+import data103 from './pyscripts/103.json'
 import data104 from './pyscripts/104.json'
+import data105 from './pyscripts/105.json'
 
 
 function App() {
+  const options = {
+      animationEnabled: true,
+      exportEnabled: true,
+      theme: "dark1",
+      // title: {text: "Data?"}
+  }
+      // data: [{type: "line", dataPoints: data102}]}
   return (
     <div className="App">
       <header className="App-header">
-        <PingScatter data={data102} label="102"/>
-        <PingScatter data={data104} label="104"/>
+        {/* <PingScatter data={data102} label="102"/> */}
+        {/* <PingScatter data={data104} label="104"/> */}
+        <CanvasJSChart options={{...options, title: {text: "102"}, data: [{type: "line", dataPoints: data102}]}}/>
+        <CanvasJSChart options={{...options, title: {text: "103"}, data: [{type: "line", dataPoints: data103}]}}/>
+        <CanvasJSChart options={{...options, title: {text: "104"}, data: [{type: "line", dataPoints: data104}]}}/>
+        <CanvasJSChart options={{...options, title: {text: "105"}, data: [{type: "line", dataPoints: data105}]}}/>
       </header>
+
+
+
     </div>
   );
 }
---
+++ src/pyscripts/ping2.py
@@ -0,0 +1,30 @@
+import os
+import json
+from time import sleep
+from datetime import datetime as dt
+
+
+ip = input("~IP addr: ")
+
+if ip == "102": ip = "172.30.112.102"
+if ip == "103": ip = "172.30.112.103"
+if ip == "104": ip = "172.30.112.104"
+
+file = input("~Filename to write to: ")
+f = open(file)
+data = json.load(f)
+
+
+while True:
+    sleep(1)
+    # 172.30.112.102
+    response = os.popen(f"ping {ip} -n 1").read().split("\n")[2]
+    time = 0
+    if 'timed out' in response:
+        time = 0
+    elif 'time=' in response:
+        time = int(response.split('time=')[1].split('ms')[0])
+    print(time)
+    data.append({"y": time, "time": dt.now().strftime("%I:%M:%S %p, %m/%d/%Y")})
+    with open(f'{file}', "w") as datafile:
+        json.dump(data, datafile)
